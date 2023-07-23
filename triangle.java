689e1702a739bbf90df885e0382df63263807cca
Rahul G <128888952+R1a2a@users.noreply.github.com>
2023-07-23 19:56:21 +0530

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+public class triangle {
+
+	
+	public static void main(String[] args) {
+		int n,i,j;
+		Scanner sc =new Scanner(System.in);
+		n=sc.nextInt();
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<i;j++)
+			{
+				System.out.println(j+1+"");
+				
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
